61f3589a2a9e1a4456562c1c3c5efbaf3819f542
Shinobidono <kouchamif@gmail.com>
2026-06-03 11:40:25 -0400

    final project phase1

---
+++ travel_recommendation.js
@@ -0,0 +1,8 @@
+function search() {
+    const keyword = document.getElementById("searchInput").value;
+    console.log("Searching for:", keyword);
+}
+
+function resetSearch() {
+    document.getElementById("searchInput").value = "";
+}
