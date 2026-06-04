a84f56139142559844548011f9ebd592d8c69903
Shinobidono <kouchamif@gmail.com>
2026-06-04 13:11:23 -0400

    final update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -24,9 +24,96 @@ async function fetchRecommendations() {
         console.log(country.name);
       });
   
-    } catch (error) {
+    } 
+    catch (error) {
       console.error('Error:', error);
     }
-  }
+}
   
-  fetchRecommendations();
+fetchRecommendations();
+
+let travelData = {};
+
+// Load JSON data once
+fetch('travel_recommendation_api.json')
+  .then(response => response.json())
+  .then(data => {
+    travelData = data;
+    console.log(data);
+  })
+  .catch(error => console.error(error));
+
+// Search button event
+document.getElementById('searchBtn').addEventListener('click', searchRecommendations);
+
+function searchRecommendations() {
+
+    const keyword = document
+        .getElementById('searchInput')
+        .value
+        .trim()
+        .toLowerCase();
+
+    let results = [];
+
+    // Beach / Beaches
+    if (keyword === 'beach' || keyword === 'beaches') {
+        results = travelData.beaches;
+    }
+
+    // Temple / Temples
+    else if (keyword === 'temple' || keyword === 'temples') {
+        results = travelData.temples;
+    }
+
+    // Country / Countries
+    else if (keyword === 'country' || keyword === 'countries') {
+        results = travelData.countries;
+    }
+
+    displayResults(results);
+}
+
+function displayResults(results) {
+
+    const resultsContainer =
+        document.getElementById('results');
+
+    resultsContainer.innerHTML = '';
+
+    if (results.length === 0) {
+        resultsContainer.innerHTML =
+            '<p>No recommendations found.</p>';
+        return;
+    }
+
+     results.forEach(place => {
+
+        const card = document.createElement('div');
+
+        card.classList.add('result-card');
+
+        card.innerHTML = `
+            <img src="${place.imageUrl}" alt="${place.name}">
+
+            <div class="result-card-content" style = "color: black;">
+                <h3>${place.name}</h3>
+
+                <p>${place.description}</p>
+
+                <button class="visit-btn">
+                    Visit
+                </button>
+            </div>
+        `;
+
+        resultsContainer.appendChild(card);
+    });
+}
+
+document.getElementById('clearBtn')
+    .addEventListener('click', () => {
+
+        document.getElementById('searchInput').value = '';
+        document.getElementById('results').innerHTML = '';
+});
